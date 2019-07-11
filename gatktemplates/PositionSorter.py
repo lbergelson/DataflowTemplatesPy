3df509b6c7b8864b41df91d9a1d3e0e53ac84bac
Louis Bergelson <louisb@broadinstitute.org>
2019-07-11 14:28:51 -0400

    Reorganize so imports are not global

--- gatktemplates/PositionSorter.py
+++ gatktemplates/PositionSorter.py
@@ -3,62 +3,25 @@ import logging
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-from apache_beam.io.gcp.internal.clients import bigquery
-from apache_beam.io.gcp import gcsio
-
-
 class MyOptions(PipelineOptions):
-    @classmethod
-    def _add_argparse_args(cls, parser):
-        parser.add_argument(
-            '--input_table',
-            type=str,
-            help='big query table to read from',
-            default='broad-dsp-spec-ops.joint_genotyping_chr20_10_exomes.pet')
-        parser.add_value_provider_argument(
-            '--output_bucket',
-            type=str,
-            help='bucket to output part files in',
-            default='gs://lb_spec_ops_test/outputs/')
-        parser.add_value_provider_argument(
-            '--other_output_bucket',
-            type=str,
-            help="other output bucket that I'm not sure what it's for",
-            default='gs://lb_spec_ops_test/outputs2/'
-        )
-
-
-def position_key_ranges(range_multiplier=200000, num_partitions=2000):
-    for positionKeyRange in [((steps - 1) * range_multiplier) for steps in range(num_partitions, 0, -1)]:
-        yield positionKeyRange
-
-
-def get_position_key_range(element):
-    position = element['position']
-
-    for i in position_key_ranges():
-        if position >= i:
-            print (str(position) + " has the range key of : " + str(i))
-            key_val = (i, position)
-            return key_val
-    print (str(position) + " has the range key of : " + str(1))
-    key_val = (0, position)
-    return key_val
-
-
-def position_sorter(key_val, output_dir):
-    key = key_val[0]
-    vals = list(key_val[1])
-    vals.sort()
-
-    out_file_path = output_dir.get() + str(key) + '.csv'
-    out_file = gcsio.GcsIO().open(out_file_path, 'w')
-    for pos in vals:
-        row = str(key) + ',' + str(pos)
-        out_file.write(row)
-        out_file.write('\n')
-    out_file.close()
-
+  @classmethod
+  def _add_argparse_args(cls, parser):
+    parser.add_argument(
+        '--input_table',
+        type=str,
+        help='big query table to read from',
+        default='broad-dsp-spec-ops.joint_genotyping_chr20_10_exomes.pet')
+    parser.add_value_provider_argument(
+        '--output_bucket',
+        type=str,
+        help='bucket to output part files in',
+        default='gs://lb_spec_ops_test/outputs/')
+    parser.add_value_provider_argument(
+        '--other_output_bucket',
+        type=str,
+        help="other output bucket that I'm not sure what it's for",
+        default='gs://lb_spec_ops_test/outputs2/'
+    )
 
 # class CustomSort(beam.DoFn):
 #     def process(self, element, *args, **kwargs):
@@ -68,32 +31,56 @@ def position_sorter(key_val, output_dir):
 
 
 def run(argv=None):
-    pipeline_options = PipelineOptions()
-    pipeline_options = pipeline_options.view_as(MyOptions)
+  from apache_beam.options.pipeline_options import PipelineOptions
+  pipeline_options = PipelineOptions()
+  pipeline_options = pipeline_options.view_as(MyOptions)
 
-    p = beam.Pipeline(options=pipeline_options)
+  p = beam.Pipeline(options=pipeline_options)
 
-    table = pipeline_options.input_table
-    table_data = (
-            p
-            | 'Query Data from BQ' >>
-            beam.io.Read(beam.io.BigQuerySource(
-                query='select * from `' + table + '`',
-                use_standard_sql=True)
-            )
-    )
+  table = pipeline_options.input_table
+  table_data = (
+      p
+      | 'Query Data from BQ' >>
+      beam.io.Read(beam.io.BigQuerySource(
+          query='select * from `' + table + '`',
+          use_standard_sql=True)
+      )
+  )
 
-    row_data = (table_data
-                | 'Generate Range Keys' >> beam.Map(lambda element: get_position_key_range(element))
-                | 'Group By Range Keys' >> beam.GroupByKey()
-                | 'Custom Local Sorter' >> beam.Map(lambda key_multi_val: position_sorter(key_multi_val, pipeline_options.output_bucket)))
+  def get_position_key_range(element):
+    range_multiplier=20000
+    position = element['position']
+    key = position // range_multiplier
+    key_val = (key, element)
+    return key_val
 
-    row_data | 'Printing data' >> beam.io.WriteToText(pipeline_options.other_output_bucket)
+  def position_sorter(key_val, output_dir):
+    from apache_beam.io.gcp import gcsio
 
-    result = p.run()
-    result.wait_until_finish()
+    key = key_val[0]
+    vals = list(key_val[1])
+    vals = sorted(vals, key=lambda x: int(x['position']))
+
+    out_file_path = output_dir.get() + "{:06d}.csv".format(key)
+    out_file = gcsio.GcsIO().open(out_file_path, 'w')
+    for pos in vals:
+      row = str(key) + ',' + str(pos['position']) + ':'+ str(pos)
+      out_file.write(row)
+      out_file.write('\n')
+    out_file.close()
+
+
+  row_data = (table_data
+              | 'Generate Range Keys' >> beam.Map(lambda element: get_position_key_range(element))
+              | 'Group By Range Keys' >> beam.GroupByKey()
+              | 'Custom Local Sorter' >> beam.Map(lambda key_multi_val: position_sorter(key_multi_val, pipeline_options.output_bucket)))
+
+  row_data | 'Printing data' >> beam.io.WriteToText(pipeline_options.other_output_bucket)
+
+  result = p.run()
+  result.wait_until_finish()
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
-    run()
+  logging.getLogger().setLevel(logging.INFO)
+  run()
